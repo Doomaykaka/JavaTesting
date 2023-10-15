aa06ff73b6a462bcafb97f893eaa853b98c937c8
Doomaykaka <doomaykaka@gmail.com>
2023-10-15 11:48:48 -0700

    Executors example fixed

--- Executor/app/src/main/java/executor/App.java
+++ Executor/app/src/main/java/executor/App.java
@@ -3,12 +3,51 @@
  */
 package executor;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        int threadsNum = 8;
+        ExecutorService service = Executors.newFixedThreadPool(threadsNum);
+        
+        Callable<String> task = () -> {
+            System.out.println(Thread.currentThread().getName());
+            return Thread.currentThread().getName();
+        };
+        
+        StringBuilder resultInLine = new StringBuilder();
+        List<Future<String>> results = new ArrayList<Future<String>>();
+        Future<String> result = null;
+        
+        for(int i = 0; i < threadsNum; i++) {
+            result = service.submit(task);
+            results.add(result);
+        }
+        
+        for(Future<String> threadResult:results) {
+            try {
+                resultInLine.append(threadResult.get(1, TimeUnit.SECONDS));
+                resultInLine.append(" | ");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        service.shutdown();
+        
+        System.out.println("-------------");
+        System.out.println(resultInLine.toString()); 
     }
 }
