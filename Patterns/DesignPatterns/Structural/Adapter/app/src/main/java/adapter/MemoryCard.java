a6ce7d30c162071a6b53e887d0a86cccd7cf54c5
Doomaykaka <doomaykaka@gmail.com>
2023-10-30 09:43:27 -0700

    Update

---
+++ Patterns/DesignPatterns/Structural/Adapter/app/src/main/java/adapter/MemoryCard.java
@@ -0,0 +1,12 @@
+package adapter;
+
+public class MemoryCard {
+
+    public void insert() {
+        System.out.println("Memory card successfully inserted!");
+    }
+
+    public void copyData() {
+        System.out.println("The data has been copied to the computer!");
+    }
+}
